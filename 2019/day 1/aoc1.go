83d2c413d4bcdb43e76a39e505be2bff8fbd648c
Manuel Romei <manuel.romei@raytecvision.com>
2019-12-01 21:25:53 +0100

    Add second part solution

--- 2019/day 1/aoc1.go
+++ 2019/day 1/aoc1.go
@@ -17,6 +17,8 @@ func main() {
 	defer file.Close()
 
 	accumulator := 0
+	accumulatorRecursive := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		mass, err := strconv.Atoi(scanner.Text())
@@ -25,15 +27,27 @@ func main() {
 		}
 
 		accumulator += fuelRequirementByMass(mass)
+		accumulatorRecursive += fuelRequirementByMassRecursive(mass)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Print("Fuel needed without taking account of fuel's fuel: ")
 	fmt.Println(accumulator)
+
+	fmt.Print("Fuel needed taking account of fuel's fuel: ")
+	fmt.Println(accumulatorRecursive)
 }
 
 func fuelRequirementByMass(mass int) int {
 	return int(math.Floor(float64(mass)/3.0) - 2.0)
+}
+
+func fuelRequirementByMassRecursive(mass int) int {
+	if fuelRequirementByMass(mass) <= 0 {
+		return 0
+	}
+	return fuelRequirementByMass(mass) + fuelRequirementByMassRecursive(fuelRequirementByMass(mass))
 }
